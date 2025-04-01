0c05b39a71a4f45638d09c349b8f1ca00f312116
Saurabh Pingale <saurabhpingale93@gmail.com>
2025-04-01 21:57:22 +0530

    commented code

--- api/app/custom_fastapi.py
+++ api/app/custom_fastapi.py
@@ -5,11 +5,11 @@ class CustmFastAPI(FastAPI):
         super().__init__(*args, **kwargs)
         
         from app.services.rag_pipeline_service import RagPipelineService
-        from app.services.store_admin_service import StoreAdminService
+        # from app.services.store_admin_service import StoreAdminService
         from app.dbhandlers.store_admin_handler import StoreAdminHandler
         from app.dbhandlers.rag_pipeline_handler import RagPipelineHandler
         
         self.rag_pipeline_service = RagPipelineService()
-        self.store_admin_service = StoreAdminService()
+        # self.store_admin_service = StoreAdminService()
         self.store_admin_handler = StoreAdminHandler()
         self.rag_pipeline_handler = RagPipelineHandler()
