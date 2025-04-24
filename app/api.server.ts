874b3e3475caf04d051906c846d91f489ae7bcc6
Saurabh Pingale <saurabhpingale93@gmail.com>
2025-04-24 20:25:15 +0530

    Added logs for debugging

--- app/api.server.ts
+++ app/api.server.ts
@@ -4,10 +4,11 @@ export async function forwardRequestToBackend(path: string, request: Request) {
 
   const headers = new Headers(request.headers);
   
-  headers.set('X-Forwarded-For', request.headers.get('CF-Connecting-IP') || '');
-  headers.set('X-Shopify-Shop-Domain', request.headers.get('X-Shopify-Shop-Domain') || '');
+  // headers.set('X-Forwarded-For', request.headers.get('CF-Connecting-IP') || '');
+  // headers.set('X-Shopify-Shop-Domain', request.headers.get('X-Shopify-Shop-Domain') || '');
 
   try {
+    console.log("Full URL:", fullUrl);
     const response = await fetch(fullUrl, {
       method: request.method,
       headers: headers,
@@ -18,21 +19,21 @@ export async function forwardRequestToBackend(path: string, request: Request) {
       const data = await response.json();
       return new Response(JSON.stringify(data), {
         status: response.status,
-        headers: {
-          'Content-Type': 'application/json',
-          'X-Shopify-API-Version': response.headers.get('X-Shopify-API-Version') || '',
-        },
+        // headers: {
+        //   'Content-Type': 'application/json',
+        //   'X-Shopify-API-Version': response.headers.get('X-Shopify-API-Version') || '',
+        // },
       });
     }
 
     return new Response(await response.text(), {
       status: response.status,
-      headers: {
-        'Content-Type': response.headers.get('content-type') || 'text/plain',
-      },
+      // headers: {
+      //   'Content-Type': response.headers.get('content-type') || 'text/plain',
+      // },
     });
   } catch (error) {
-    console.error('Proxy request failed:', error);
+    console.error('Proxy request failed1111:', error);
     const errorData = {
       error: 'Failed to process request',
       details: error instanceof Error ? error.message : 'Unknown error',
