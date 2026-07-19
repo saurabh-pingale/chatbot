024c2846cbeed138b76eb9490c74040fa549f8a5
BalanaguYashwanth <balanaguyashwanth@gmail.com>
2026-07-19 20:37:35 +0530

    update the shopify bridge cdn

--- app/root.tsx
+++ app/root.tsx
@@ -1,17 +1,29 @@
+import type { LoaderFunctionArgs } from "@remix-run/node";
 import {
   Links,
   Meta,
   Outlet,
   Scripts,
   ScrollRestoration,
+  useLoaderData,
 } from "@remix-run/react";
 
+export const loader = async (_args: LoaderFunctionArgs) => {
+  return {
+    apiKey: process.env.SHOPIFY_API_KEY || "",
+  };
+};
+
 export default function App() {
+  const { apiKey } = useLoaderData<typeof loader>();
+
   return (
     <html>
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <meta name="shopify-api-key" content={apiKey} />
+        <script src="https://cdn.shopify.com/shopifycloud/app-bridge.js"></script>
         <link rel="preconnect" href="https://cdn.shopify.com/" />
         <link
           rel="stylesheet"
